b43bf5184f71e6866df3cbca423424f78bdeab86
Dung Nguyen <nguyentridung165@gmail.com>
2020-04-02 03:18:07 +0700

    - Done bitcoin-ticker challenge

--- bitcoin-ticker-flutter/lib/coin_data.dart
+++ bitcoin-ticker-flutter/lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,22 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const apiKey = 'A6A9C7FE-2EF5-46AD-8681-C975BF68ACDC';
+
+class CoinData {
+
+  CoinData();
+
+  Future getCoinData(String coin, String currency) async {
+    http.Response response = await http.get(
+        'https://rest.coinapi.io/v1/exchangerate/$coin/$currency?apikey=$apiKey');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+      print(response.body);
+    }
+  }
+}
--- bitcoin-ticker-flutter/lib/price_screen.dart
+++ bitcoin-ticker-flutter/lib/price_screen.dart
@@ -4,12 +4,47 @@ import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
+
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = currenciesList.first;
+
+  int BTCRate = 0;
+  int ETHRate = 0;
+  int LTCRate = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    updateRate();
+  }
+
+  void updateRate() async {
+    CoinData coinData = CoinData();
+    for (String coin in cryptoList) {
+      var data = await coinData.getCoinData(coin, selectedCurrency);
+      setState(() {
+        double tempRate = data['rate'];
+        switch (coin) {
+          case 'BTC':
+            BTCRate = tempRate.toInt();
+            break;
+
+          case 'ETH':
+            ETHRate = tempRate.toInt();
+            break;
+
+          case 'LTC':
+            LTCRate = tempRate.toInt();
+            break;
+        }
+      });
+    }
+
+  }
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> list = [];
@@ -25,6 +60,10 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
+          BTCRate = 0;
+          ETHRate = 0;
+          LTCRate = 0;
+          updateRate();
         });
       },
     );
@@ -40,7 +79,11 @@ class _PriceScreenState extends State<PriceScreen> {
       backgroundColor: Colors.lightBlue,
       itemExtent: 32.0,
       onSelectedItemChanged: (index) {
-
+        selectedCurrency = currenciesList[index];
+        BTCRate = 0;
+        ETHRate = 0;
+        LTCRate = 0;
+        updateRate();
       },
       children: list,
     );
@@ -67,7 +110,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $BTCRate $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -77,14 +120,58 @@ class _PriceScreenState extends State<PriceScreen> {
               ),
             ),
           ),
+
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 ETH = $ETHRate $selectedCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 LTC = $LTCRate $selectedCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
-          ),
-        ],
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),)
+          ],
       ),
     );
   }
